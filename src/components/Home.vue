1f29e032ece826f953eff0b3d27e62be8d04d3dd
Ingrid Banguero <ingrid_banguero@hotmail.com>
2023-10-12 19:43:28 -0500

    Create header and content layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,20 @@
 <template>
-    <div>Hola mundo</div>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+    </Layout>
 </template>
+
+
+<script>
+    import Layout from "@/components/Layout.vue";
+    import Header from "./Header.vue";
+    export default {
+        name: "Home",
+        components: {
+            Layout,
+            Header
+        }
+    }
+</script>
