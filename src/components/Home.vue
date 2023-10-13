602cce321f5ee66adf870f6795cdf0d53ec97362
Ingrid Banguero <ingrid_banguero@hotmail.com>
2023-10-12 22:25:43 -0500

    Crate history

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,18 +3,107 @@
         <template #header>
             <Header></Header>
         </template>
+        <template #resume>
+            <Resumen
+                :total-label="'Monto total'"
+                :label="label"
+                :total-amount="1000000"
+                :amount="amount"
+            >
+                <template #graphic>
+                    graphic
+                </template>
+
+                <template #action>
+                    actions
+                </template>
+            </Resumen>
+        </template>
+        <template #movements>
+            <Movements
+                :movements="movements"
+            
+            />
+        </template>
     </Layout>
 </template>
 
 
 <script>
     import Layout from "@/components/Layout.vue";
-    import Header from "./Header.vue";
+    import Header from "@/components/Header.vue";
+    import Resumen from "@/components/Resumen.vue";
+    import Movements from "@/components/Movements.vue";
     export default {
         name: "Home",
         components: {
             Layout,
-            Header
-        }
+            Header,
+            Resumen,
+            Movements
+        },
+        data() {
+            return {
+                amount: null,
+                label: null,
+                movements: [
+                    {
+                        id: 1,
+                        title: "Movimiento 1",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 1000,
+                    },
+                    {
+                        id: 2,
+                        title: "Movimiento 2",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 2000,
+                    },
+                    {
+                        id: 3,
+                        title: "Movimiento 3",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 3000,
+                    },
+                    {
+                        id: 4,
+                        title: "Movimiento 4",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 1000,
+                    },
+                    {
+                        id: 5,
+                        title: "Movimiento 5",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 2000,
+                    },
+                    {
+                        id: 6,
+                        title: "Movimiento 6",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 3000,
+                    },
+                    {
+                        id: 7,
+                        title: "Movimiento 7",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 1000,
+                    },
+                    {
+                        id: 8,
+                        title: "Movimiento 8",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 2000,
+                    },
+                    {
+                        id: 9,
+                        title: "Movimiento 9",
+                        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                        amount: 3000,
+                    }
+            
+                ]
+            }
+        },
     }
 </script>
---
+++ src/components/Resumen.vue
@@ -0,0 +1,75 @@
+<template>
+    <main>
+        <p>{{ labelVisual }}</p>
+        <h1>{{ amountCurrency }}</h1>
+        <div class="graphic">
+            <slot name="graphic"></slot>
+        </div>
+        <div class="actions">
+            <slot name="actions"></slot>
+        </div>
+    </main>
+</template>
+
+<script>
+
+    const currencyFormatter = new Intl.NumberFormat('es-CO', { 
+        style: 'currency', currency: 'COP' });
+    
+    export default {
+        name: "Resumen",
+        props: {
+            totalLabel: {
+                type: String,
+            },
+            label: {
+                type: String,
+            },
+            totalAmount: {
+                type: Number,
+            },
+            amount: {
+                type: Number,
+                default: null
+            }
+        },
+        computed: {
+            amountVisual() {
+                return this.amount !== null ? this.amount : this.totalAmount;
+            },
+            labelVisual() {
+                return this.amount !== null ? this.label : this.totalLabel;
+            },
+            amountCurrency() {
+                return currencyFormatter.format(this.amountVisual);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h1,
+p {
+  margin: 0;
+  text-align: center;
+}
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
+</style>
