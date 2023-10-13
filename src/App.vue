c42bbaff9782a2d26e25da09cc1eefb2b40abc28
Ingrid Banguero <ingrid_banguero@hotmail.com>
2023-10-12 19:26:37 -0500

    Create splash screen

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
-  <Home />
+  <Suspense> <!-- Componente que recibe dos slots -->
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-  import Home from "@/components/Home.vue";
-
+  import SplashScreen from "@/components/SplashScreen.vue";
+  import Home from "@/components/Home.vue"; 
+  import { defineAsyncComponent } from "vue";
+  
   // Composition API
   export default {
     name: "App",
     components: {
-      Home
+      SplashScreen,
+      Home: defineAsyncComponent(() => 
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import("@/components/Home.vue"));
+        }, 2500);
+      })
+      ),
     }
   }
 </script>
 
 <style>
+  .app {
+    min-height: 100vh;
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
 
+  * {
+    --brand-green: #04b500;
+    --brand-blue: #0689b0;
+  }
 </style>
